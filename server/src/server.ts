5e92a98fbef6b52b7bbcf22c89cd5e8772139927
GabouKing <gribeirodelimas2@gmail.com>
2023-07-22 20:44:25 -0300

    primeiro commit

---
+++ server/src/server.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { fastify } from 'fastify'
+import { resolve } from 'path'
+
+import { authRoutes } from './routes/auth'
+import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
+
+const app = fastify()
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(cors, {
+  origin: true, // todas as URLs de frontend que poderão acessar a api
+})
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚄 HTTP server running on http://localhost:3333')
+  })
